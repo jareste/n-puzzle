2e3bc2e596786a567d16bf128bdda6339ff045e3
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-02-08 12:17:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+First steps:
+- Open a file to read.
+- Build a struct with int** to save the parser.
+    Struct:
+        int **game;
+        int size;
+        int mode;
---
+++ srcs/main.rs
@@ -0,0 +1,5 @@
+// This is your main function
+fn main() {
+    // Statements here are executed when the compiled binary is called
+    println!("Hello, World!"); // Print text to the console
+}
