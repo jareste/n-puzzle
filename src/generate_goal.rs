d07fc9f25a71acdb50c907b1afeed853a3e5dfa9
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-02-10 14:39:19 +0100

    generator for goal done

---
+++ src/generate_goal.rs
@@ -0,0 +1,47 @@
+pub struct Map {
+    pub matrix: Vec<Vec<usize>>,
+    pub x: isize,
+    pub y: isize,
+    pub size: usize,
+}
+
+pub fn generate_goal(size: usize) -> Map {
+    let mut map = Map {
+        matrix: vec![vec![0; size]; size],
+        x: 0,
+        y: 0,
+        size: size,
+    };
+    let mut counter = 0;
+
+    let mut x: isize = 0;
+    let mut y: isize = 0;
+    let mut dx: isize = 0;
+    let mut dy: isize = 1;
+
+    while counter < size * size {
+        if x >= 0 && x < size as isize && y >= 0 && y < size as isize {
+            counter += 1;
+            map.matrix[x as usize][y as usize] = counter;
+            map.x = x;
+            map.y = y;
+        }
+
+        if x + dx >= size as isize || x + dx < 0 || y + dy >= size as isize || y + dy < 0 || map.matrix[(x + dx) as usize][(y + dy) as usize] != 0 {
+            let temp = dx;
+            dx = dy;
+            dy = -temp;
+        }
+
+        x += dx;
+        y += dy;
+    }
+
+    map.matrix[map.x as usize][map.y as usize] = 0;
+
+    println!("Goal:");
+    for i in 0..size {
+        println!("{:?}", map.matrix[i]);
+    }
+    map
+}
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,3 @@
-use std::fs;
-
 pub struct Parser {
     pub map: Vec<Vec<i16>>,
     pub size: usize,
@@ -7,69 +5,66 @@ pub struct Parser {
 
 #[derive(Debug)]
 pub enum ParserError {//hacer un enum que devulva parser o error
-    FileNotFound,
     FileNotReadable,
     SizeTooLarge,
     InvalidFormat,
 }
 
-impl Parser {
-    pub fn parse_file(contents: String) -> Result<Parser, ParserError> {
+pub fn parse_file(contents: String) -> Result<Vec<Vec<i16>>, ParserError> {
 
-        let mut lines = contents.lines();
+    let mut lines = contents.lines();
 
-        let mut first_line = match lines.next() {
+    let mut first_line = match lines.next() {
+        Some(line) => line,
+        None => {
+            println!("Error: File is empty.");
+            return Err(ParserError::FileNotReadable)},
+    };
+
+    if first_line == "# This puzzle is solvable" || first_line == "# This puzzle is unsolvable" {
+        first_line = match lines.next() {
             Some(line) => line,
             None => {
                 println!("Error: File is empty.");
                 return Err(ParserError::FileNotReadable)},
         };
-
-        if first_line == "# This puzzle is solvable" || first_line == "# This puzzle is unsolvable" {
-            first_line = match lines.next() {
-                Some(line) => line,
-                None => {
-                    println!("Error: File is empty.");
-                    return Err(ParserError::FileNotReadable)},
-            };
-        }
-
-        
-
-        let size: usize = match first_line.trim().parse() {
-            Ok(num) => num,
-            Err(_) => return Err(ParserError::FileNotReadable),
-        };
-
-        // println!("size: {}", size);
-        if size > 25 {
-            return Err(ParserError::SizeTooLarge);
-        }
-
-        let mut parser = Parser {
-            map: vec![vec![0i16; size]; size],
-            size: size,
-        };
-        
-        let mut row = 0;
-        for line in lines {
-            let numbers: Vec<&str> = line.split_whitespace().collect();
-            if numbers.len() != size {
-                return Err(ParserError::InvalidFormat);
-            }
-            
-            let mut col = 0;
-            for num_str in numbers {
-                match num_str.parse::<i16>() {
-                    Ok(num) => {
-                        parser.map[row][col] = num;
-                        col += 1;
-                    }
-                    Err(_) => return Err(ParserError::InvalidFormat),
-                }
-            }
-            row += 1;
-        }
-        Ok(parser)
     }
+
+    
+
+    let size: usize = match first_line.trim().parse() {
+        Ok(num) => num,
+        Err(_) => return Err(ParserError::FileNotReadable),
+    };
+
+    // println!("size: {}", size);
+    if size > 25 {
+        return Err(ParserError::SizeTooLarge);
+    }
+
+    let mut parser = Parser {
+        map: vec![vec![0i16; size]; size],
+        size: size,
+    };
+    
+    let mut row = 0;
+    for line in lines {
+        let numbers: Vec<&str> = line.split_whitespace().collect();
+        if numbers.len() != size {
+            return Err(ParserError::InvalidFormat);
+        }
+        
+        let mut col = 0;
+        for num_str in numbers {
+            match num_str.parse::<i16>() {
+                Ok(num) => {
+                    parser.map[row][col] = num;
+                    col += 1;
+                }
+                Err(_) => return Err(ParserError::InvalidFormat),
+            }
+        }
+        row += 1;
+    }
+    Ok(parser.map)
 }
