a82b15bf1094f511977d623e037984c144d56f46
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-02-17 17:57:56 +0100

    changed ** per * on noadmisible heuristic

--- src/idastar.rs
+++ src/idastar.rs
@@ -53,7 +53,7 @@ fn search(path: & mut Vec<Map>, g: usize, bound: usize, goal: &Map, heuristic: &
         solver::Heuristic::Hamming => node.hamming_dist(goal),
         solver::Heuristic::Euclidean => node.euclidean_dist(goal),
         solver::Heuristic::LinearConflicts => node.manhattan_linear_conflicts(goal),
-        solver::Heuristic::NoAdmisible => node.manhattan_dist(goal) * node.manhattan_dist(goal),
+        solver::Heuristic::NoAdmisible => node.manhattan_dist(goal) * 4,
     };
 
     let f = match h_method{
